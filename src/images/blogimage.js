e2d6e717c6e089afe6ab1deb370519e8d4d518e0
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-12 20:42:00 -0400

    cursive style added

--- src/images/blogimage.js
+++ src/images/blogimage.js
@@ -3,8 +3,11 @@ import "bulma/css/bulma.css"
 const blogimage = () => {
     return(
         <div className="container">
-            <img src="https://previews.123rf.com/images/tommoh29/tommoh291704/tommoh29170400249/76404670-vintage-style-photo-blog-background-or-template.jpg"
+           <img src="https://previews.123rf.com/images/tommoh29/tommoh291704/tommoh29170400249/76404670-vintage-style-photo-blog-background-or-template.jpg"
                     height = "40px" width = "1000px"  />
+    
+                  
+                
             <div >This is my blog.</div>
          
         </div>
